37f68b8806cda7a74a067368e163464d85f86288
Nick Davis <ndavis55862@gmail.com>
2025-06-15 23:32:58 -0700

    Add main loading screen for video

--- nde-site/src/pages/Main.tsx
+++ nde-site/src/pages/Main.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion, useScroll, useTransform } from 'framer-motion';
+
+import logo from "../assets/images/logo-white-transparent.png"
 
 export default function Main() {
   const { scrollY } = useScroll();
   const scale = useTransform(scrollY, [0, 500], [1.2, 0.91]);
   const opacity = useTransform(scrollY, [0, 300], [1, 0.3]);
+  const [videoLoaded, setVideoLoaded] = useState(false);
 
   return (
     <section id='main' className="h-[200vh] relative">
+      {!videoLoaded && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-primary">
+          <img className="text-xl animate-pulse" src={logo} alt="ND Enterprises" />
+        </div>
+      )}
       <div className="sticky top-0 -mt-20 h-screen w-full flex items-center justify-center overflow-hidden">
         <motion.video
           src="/assets/warehouse.mp4"
@@ -17,8 +25,9 @@ export default function Main() {
           muted
           playsInline
           style={{ scale, opacity }}
+          onLoadedData={() => setVideoLoaded(true)}
         />
-        <div className="absolute w-3/4 text-center z-10 text-white px-4">
+        <div className="absolute w-3/4 text-center z-10 px-4">
             <p className="text-3xl">ND ENTERPRISES</p>
             <p className="text-5xl md:text-7xl font-bold mt-2">Your Products, Delivered with Precision.</p>
         </div>
